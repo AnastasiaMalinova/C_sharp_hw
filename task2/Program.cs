3f06bf0aea90df1d73e188e4e4f58141dfc00f48
Anastasia Malinova <anamali94@mail.ru>
2022-08-15 09:37:46 +0300

    task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2
+int a = 5;
+int b = 7;
+
+int max = a;
+if (b > max ) 
+{
+    max = b;
+    Console.Write("max = ");
+    Console.Write(max);
+} 
+else 
+{
+    Console.Write("max = ");
+    Console.Write(max);
+}
+
+
