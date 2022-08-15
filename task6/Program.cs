4bd734a31721d91f0563af60b417cdb51e2f8249
Anastasia Malinova <anamali94@mail.ru>
2022-08-15 10:53:43 +0300

    task6 added

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6
+Console.Write("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+if (number %2 == 0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
