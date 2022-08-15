5b7d068721fe0fc72e5f132ce4f4d33372b7248c
Anastasia Malinova <anamali94@mail.ru>
2022-08-15 11:53:08 +0300

    task 8 added

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8
+Console.Write("Введите число: ");
+int N  = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while(number <= N)
+{
+   if (number % 2 == 0) 
+   {
+    Console.WriteLine(number);
+   }
+   number += 1;
+}        
